e2774d5d8d75171d1ac82f2f28cbff6eccb77624
Danny Engelman <danny@engelman.nl>
2024-01-06 11:38:29 +0100

    v1

--- element.js
+++ element.js
@@ -1,22 +1,12 @@
-# xy-component.github.io
-&lt;xy-component> native Web Component displaying X,Y mouse coordinates
-
-```html	
-  <xy-component></xy-component>
-```
-
-## Using Native JavaScript
-
-```javascript
 customElements.define("xy-component", class extends HTMLElement {
     constructor(
-        x = document.createElement("b"), 
+        x = document.createElement("b"),
         y = document.createElement("b")
     ) {
         super()
             .attachShadow({ mode: "open" })
             .append("Mouse X: ", x, " Y: ", y);
-        
+
         Object.defineProperties(this, {
             x: {
                 get: v => Number(x.innerHTML),
@@ -27,15 +17,10 @@ customElements.define("xy-component", class extends HTMLElement {
                 set: v => y.innerHTML = v
             }
         });
-        
+
         window.addEventListener("mousemove", e => (
             this.x = e.clientX,
             this.y = e.clientY
         ));
     }
 });
-```
-
-## Using StencilJS (+18Kb)
-
-![](stencil_state.jpg)
